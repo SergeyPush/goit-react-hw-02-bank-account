3ff8c018b2d46f0a8deb7ca433d083dd9fbeaebd
Sergey Pushkovskiy <push_s@mail.ua>
2019-10-17 21:51:05 +0300

    Updates

---
+++ precache-manifest.31d313887b50aa3c1524565e5047c299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ecf41b560bddcca97ef606f853c640",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "f817989b66305051f1e0",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.da529cc7.chunk.css"
+  },
+  {
+    "revision": "38f89b2ae8fa61e93345",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.1e54a37a.chunk.css"
+  },
+  {
+    "revision": "f817989b66305051f1e0",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.4f22f869.chunk.js"
+  },
+  {
+    "revision": "38f89b2ae8fa61e93345",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.c89c7a06.chunk.js"
+  },
+  {
+    "revision": "4456fcf01a6249bbb0da",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.b10a97a6.js"
+  }
+]);
